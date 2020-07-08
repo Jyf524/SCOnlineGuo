bf8cb7fa1de9bd1e6cbda647614b51b684927258
Jyf524 <806398394@qq.com>
2020-07-08 08:12:18 +0800

    first commit

---
+++ SCOnline/Controllers/ForeMentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCOnline.Controllers
+{
+    public class ForeMentController : Controller
+    {
+        //
+        // GET: /ForeMent/
+
+        public ActionResult ElectiveCenter()
+        {
+            return View();
+        }
+        public ActionResult ElectiveDetail()
+        {
+            return View();
+        }
+        public ActionResult NewsDetail()
+        {
+            return View();
+        }
+    }
+}
